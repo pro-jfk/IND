68edf641161c12a0c98bc1f1fa3ea4e455cf3b92
JFK <jasrajkhedoe@outlook.com>
2022-11-14 21:29:55 +0100

    ( not necessary) added ModelBuilder

--- Data/INDContext.cs
+++ Data/INDContext.cs
@@ -5,11 +5,27 @@ namespace Data;
 
 public class IndContext : DbContext
 {
-    // public Context (DbContextOptions<Context>options) : base(options){}
+    public IndContext (DbContextOptions<IndContext>options) : base(options){}
     
     public DbSet<Customer> Customers { get; set; }
     public DbSet<EmergencyShelter> EmergencyShelters { get; set; }
     public DbSet<Message> Messages { get; set; }
     public DbSet<Pole> Poles { get; set; }
 
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     modelBuilder.Entity<Customer>().HasData(
+    //         new Customer()
+    //         {
+    //             //CustomerId = "v_number",
+    //             LastName = "last_name",
+    //             MiddleName = "middle_name",
+    //             FirstName = "first_name",
+    //             Origin = "origin",
+    //             DateAdded = Convert.ToDateTime("date_of_birth")
+    //             
+    //              
+    //         }
+    //     );
+    //}
 }
