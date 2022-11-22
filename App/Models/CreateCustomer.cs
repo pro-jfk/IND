19e79770ae3fef4d391de207a0ffe177e71161ac
JFK <jasrajkhedoe@outlook.com>
2022-11-22 14:28:52 +0100

    Added phonenumber and Languagesspoken attributes

--- App/Models/CreateCustomer.cs
+++ App/Models/CreateCustomer.cs
@@ -8,7 +8,7 @@ public class CreateCustomer
     public string MiddleName { get; set; }
     public string FirstName { get; set; }
     public string Origin { get; set; }
-    public string Language { get; set; }
+    public string LanguagesSpoken { get; set; }
     public string PhoneNumber { get; set; }
     public int LocationID { get; set; }
 }
--- Core/Entities/Customer.cs
+++ Core/Entities/Customer.cs
@@ -13,7 +13,7 @@ public class Customer
     public string MiddleName { get; set; }
     public string FirstName { get; set; }
     public string Origin { get; set; }
-    public string Language { get; set; }
+    public string LanguagesSpoken { get; set; }
     public string PhoneNumber { get; set; }
     public DateTime DateAdded { get; set; }
   
