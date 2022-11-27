38f0fe1ec9bac4888d4b4e3f41d15b7984e7d4dc
JFK <jasrajkhedoe@outlook.com>
2022-11-27 15:03:52 +0100

    Removed middle name field

--- Core/Entities/Customer.cs
+++ Core/Entities/Customer.cs
@@ -10,7 +10,6 @@ public class Customer
     public int Id { get; set; }
     
     public string LastName { get; set; }
-    public string MiddleName { get; set; }
     public string FirstName { get; set; }
     public string Origin { get; set; }
     public string LanguagesSpoken { get; set; }
