15e4988c3e838ca2d0a080db57eed0df1a6c3849
JFK <jasrajkhedoe@outlook.com>
2022-12-12 10:15:25 +0100

    configured mapper profile for CustomerMessage

---
+++ App/MappingProfile/CustomerMessageProfile.cs
@@ -0,0 +1,19 @@
+﻿using App.Extensions;
+using App.Models;
+using App.Responses;
+using AutoMapper;
+using Core.Entities;
+
+
+public class CustomerMessageProfile : Profile
+{
+    public CustomerMessageProfile()
+    {
+        CreateMap<CreateCustomerMessage, CustomerMessage>()
+            .IgnoreDestination(cm => cm.DateReceived)
+            .IgnoreDestination(cm =>cm.MessageId);
+        
+        CreateMap<CustomerMessage, CustomerMessageResponse>();
+    }
+}
+
