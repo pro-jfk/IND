6d1c6c9a1b686122bb685bd3acedc5061be2721d
JFK <jasrajkhedoe@outlook.com>
2022-12-13 15:07:44 +0100

    removed GettAllAsync parameter

--- Data/Repositories/ICustomerMessageRepository.cs
+++ Data/Repositories/ICustomerMessageRepository.cs
@@ -7,7 +7,7 @@ using Core.Common;
 public interface ICustomerMessageRepository
 {
     public Task<CustomerMessage> GetFirstASync(Expression<Func<CustomerMessage, bool>> predicate);
-    public Task<List<CustomerMessage>> GetAllAsync(Expression<Func<CustomerMessage, bool>> predicate);
+    public Task<List<CustomerMessage>> GetAllAsync();
     public Task<CustomerMessage> AddAsync(CustomerMessage customerMessage);
     public Task<CustomerMessage> UpdateAsync(CustomerMessage customerMessage);
     public Task<CustomerMessage> DeleteAsync(CustomerMessage customerMessage);
