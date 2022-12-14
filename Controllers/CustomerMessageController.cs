dfe311f2eadd5562da6ecfcaf97de0a8c06b7eef
JFK <jasrajkhedoe@outlook.com>
2022-12-14 10:36:54 +0100

    added Patch methods

--- Controllers/CustomerMessageController.cs
+++ Controllers/CustomerMessageController.cs
@@ -7,19 +7,23 @@ namespace API.Controllers;
 
 public class CustomerMessageController : ApiController
 {
-    [HttpGet]
-    public async Task<string[]> GetMessage()
-    {
-        string[] test = { "hello", " world", "!" };
-        return test;
-    }
 
-    [HttpPost]  
-    public async Task<IActionResult> CreateCustomerMessage([FromServices] ICustomerMessageService customerMessageService,
-        CreateCustomerMessage createCustomerMessage)
+    [HttpPatch]  
+    public async Task<IActionResult> PatchCustomerMessagePrint([FromServices] ICustomerMessageService customerMessageService,
+        int customerId, int messageId)
     {
-        CustomerMessageResponse? customerMessage = await customerMessageService.CreateCustomerMessage(createCustomerMessage);
+        CustomerMessageResponse? customerMessage = await customerMessageService.UpdateCustomerMessagePrint(customerId, messageId);
         ApiResult<CustomerMessageResponse> result = ApiResult<CustomerMessageResponse>.Succes(customerMessage);
         return Ok(result);
     }
+
+    [HttpPatch]
+    public async Task<IActionResult> PatchCustomerMessageReceived([FromServices] ICustomerMessageService customerMessageService,
+        int customerId, int messageId)
+    {
+        CustomerMessageResponse customerMessage = await customerMessageService.UpdateCustomerMessageReceived(customerId, messageId);
+        ApiResult<CustomerMessageResponse> result = ApiResult<CustomerMessageResponse>.Succes(customerMessage);
+        return Ok(result);
+    }
+
 }
