05398043f3d5ed4ebe9698a3ba573bff1b6895c9
JFK <jasrajkhedoe@outlook.com>
2022-12-14 11:02:37 +0100

    implemented get controller

--- Controllers/CustomerMessageController.cs
+++ Controllers/CustomerMessageController.cs
@@ -1,12 +1,22 @@
 ﻿using App.Models;
 using App.Responses;
 using App.Services;
+using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 public class CustomerMessageController : ApiController
 {
+    [HttpGet]
+    public async Task<IActionResult> GetCustomerMessage([FromServices] ICustomerMessageService customerMessageService, int customerId,
+        int messageId)
+    {
+        CustomerMessageResponse customerMessage = await customerMessageService.GetCustomerMessage(customerId, messageId);
+        ApiResult<CustomerMessageResponse> result =  ApiResult<CustomerMessageResponse>.Succes(customerMessage);
+        return Ok(result);
+    }
+
 
     [HttpPatch]  
     public async Task<IActionResult> PatchCustomerMessagePrint([FromServices] ICustomerMessageService customerMessageService,
@@ -25,5 +35,6 @@ public class CustomerMessageController : ApiController
         ApiResult<CustomerMessageResponse> result = ApiResult<CustomerMessageResponse>.Succes(customerMessage);
         return Ok(result);
     }
+    
 
 }
