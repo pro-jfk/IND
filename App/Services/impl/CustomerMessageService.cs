e9530c1650cfb0e9ce312dbba33be48618fa09b7
JFK <jasrajkhedoe@outlook.com>
2022-12-19 15:24:16 +0100

    code refactoring

--- App/Services/impl/CustomerMessageService.cs
+++ App/Services/impl/CustomerMessageService.cs
@@ -14,27 +14,22 @@ public class CustomerMessageService : ICustomerMessageService
     private readonly IMapper _mapper;
     
     //PrintNode API token and API
-    private  HttpClient sharedClient = new()
+    private readonly HttpClient _sharedClient = new()
     {
         BaseAddress = new Uri("https://api.printnode.com/printjobs")
     };
     
-    private readonly string ApiToken = "cX6VaQJ61hZDim6R-0dMLGdfvMwN1tiQ0Wuv2MilRgs";
+    private readonly string _apiToken = "cX6VaQJ61hZDim6R-0dMLGdfvMwN1tiQ0Wuv2MilRgs";
     
 
-    public CustomerMessageService(ICustomerMessageRepository customerMessageRepository, IMapper mapper)
+    public CustomerMessageService (ICustomerMessageRepository customerMessageRepository, IMapper mapper)
     {
         _customerMessageRepository = customerMessageRepository;
         _mapper = mapper;
     }
     //Create CustomerMessage
-    public async Task<CustomerMessageResponse> CreateCustomerMessage(int customerId, int messageId)
+    public async Task<CustomerMessageResponse> CreateCustomerMessage(CreateCustomerMessage createCustomerMessage)
     {
-        CreateCustomerMessage createCustomerMessage = new CreateCustomerMessage
-        {
-            CustomerId = customerId,
-            MessageId = messageId
-        };
         CustomerMessage customerMessage = _mapper.Map<CustomerMessage>(createCustomerMessage);
         CustomerMessage result = await _customerMessageRepository.AddAsync(customerMessage);
         
@@ -58,11 +53,11 @@ public class CustomerMessageService : ICustomerMessageService
     }
     
     //Update CustomerMessage Print Details
-    public async Task<CustomerMessageResponse> UpdateCustomerMessagePrint(int customerId, int messageId)
+    public async Task<CustomerMessageResponse> UpdateCustomerMessagePrintJob(int customerId, int messageId)
     {
         CustomerMessage customerMessage = await _customerMessageRepository.GetFirstASync(cm => cm.CustomerId == customerId && cm.MessageId == messageId );        customerMessage.TimesPrinted += 1;
 
-        if (customerMessage.TimesPrinted >= 1)
+        while (customerMessage.TimesPrinted >= 1)
         {
             customerMessage.StatusPrinted = true;
         }
@@ -72,13 +67,23 @@ public class CustomerMessageService : ICustomerMessageService
     
     //Update CustomerMessage Received Details
 
-    public async Task<CustomerMessageResponse> UpdateCustomerMessageReceived(int customerId, int messageId)
+    public async Task<CustomerMessageResponse> UpdateCustomerMessageReceived(int customerId, int messageId, DateTime dateTime)
+    {
+        CustomerMessage customerMessage = await _customerMessageRepository.GetFirstASync(cm => cm.CustomerId == customerId && cm.MessageId == messageId );
+        //customerMessage = _mapper.Map<CustomerMessage>();
+        customerMessage.DateReceived = dateTime;
+        customerMessage.StatusReceived = true;
+        CustomerMessage result = await _customerMessageRepository.UpdateAsync(customerMessage);
+        return _mapper.Map<CustomerMessageResponse>(result);
+    }
+
+    public async Task<CustomerMessageResponse> DeleteCustomerMessage(int customerId, int messageId)
     {
         CustomerMessage customerMessage = await _customerMessageRepository.GetFirstASync(cm => cm.CustomerId == customerId && cm.MessageId == messageId );
         //customerMessage = _mapper.Map<CustomerMessage>();
         customerMessage.StatusReceived = true;
         customerMessage.DateReceived = DateTime.Now;
-        CustomerMessage result = await _customerMessageRepository.UpdateAsync(customerMessage);
+        CustomerMessage result = await _customerMessageRepository.DeleteAsync(customerMessage);
         return _mapper.Map<CustomerMessageResponse>(result);
     }
 
@@ -86,14 +91,14 @@ public class CustomerMessageService : ICustomerMessageService
     //Docs: https://www.printnode.com/en/docs/api/curl
     public async void PrintMessage()
     {
-        var bytes = Encoding.UTF8.GetBytes(ApiToken);
+        var bytes = Encoding.UTF8.GetBytes(_apiToken);
         string encodeUri = Convert.ToBase64String(bytes);
         
-        sharedClient.DefaultRequestHeaders.Authorization =
+        _sharedClient.DefaultRequestHeaders.Authorization =
             new AuthenticationHeaderValue("Basic", encodeUri);
 
-        var bytes_test = Encoding.UTF8.GetBytes("hello");
-        string testContent = Convert.ToBase64String(bytes_test);
+        var bytesTest = Encoding.UTF8.GetBytes("hello");
+        string testContent = Convert.ToBase64String(bytesTest);
         
         var values = new Dictionary<string, string>
         {
@@ -106,13 +111,15 @@ public class CustomerMessageService : ICustomerMessageService
             {"fit_to_page", "true"}
         };
         var content = new FormUrlEncodedContent(values);
-        using var response = await sharedClient.PostAsync(sharedClient.BaseAddress, content);
+        using var response = await _sharedClient.PostAsync(_sharedClient.BaseAddress, content);
         var responseString = await response.Content.ReadAsStringAsync();
 
         Console.WriteLine(responseString);
         var jsonResponse = await response.Content.ReadAsStringAsync();
         Console.WriteLine($"{jsonResponse}\n");
     }
-    public CustomerMessageResponse NotImplementedException { get; }
+/*
+    public CustomerMessageResponse? NotImplementedException { get; }
+*/
     
 }
