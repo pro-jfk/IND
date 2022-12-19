0c45937a0d319763becfff14775a66e3297fb519
JFK <jasrajkhedoe@outlook.com>
2022-12-19 15:20:04 +0100

    added optional customization for compoundkey

--- Data/INDContext.cs
+++ Data/INDContext.cs
@@ -10,20 +10,30 @@ public class IndContext : DbContext
     /// A DbContext instance represents a session with the database and can be used to query and save instances of your entities
     /// </summary>
     /// <param name="options"></param>
-    //TODO Run migrations for TimesPrinted field in Customer_Message
+   
     public IndContext (DbContextOptions<IndContext>options) : base(options){}
     
     public DbSet<Customer> Customers { get; set; }
     public DbSet<EmergencyShelter> EmergencyShelters { get; set; }
     public DbSet<Message> Messages { get; set; }
     public DbSet<Pole> Poles { get; set; }
-    public DbSet<CustomerMessage> CustomerMessage { get; set; }
+    public DbSet<CustomerMessage> CustomerMessages{ get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<CustomerMessage>()
-            .HasKey(t => new{t.CustomerId,t.MessageId}); //Creates Compound PKey for joined table Customer_Messages
-            
+            .HasKey(cm => new{cm.CustomerId,cm.MessageId}); //Creates Compound PKey for joined table Customer_Messages
+        
+        // modelBuilder.Entity<CustomerMessage>()
+        //     .HasOne<Customer>(cm => cm.Customer)
+        //     .WithMany(c => c.CustomerMessages)
+        //     .HasForeignKey(cm => cm.CustomerId);
+        //
+        //
+        // modelBuilder.Entity<CustomerMessage>()
+        //     .HasOne<Message>(cm => cm.Message)
+        //     .WithMany(c => c.CustomerMessages)
+        //     .HasForeignKey(cm => cm.MessageId);
         
     }
 }
