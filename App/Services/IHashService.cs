fc735d73736b9fb7b6828c4182829b891ec081a3
JFK <jasrajkhedoe@outlook.com>
2023-01-08 21:20:06 +0100

    added hashing

---
+++ App/Services/IHashService.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Services;
+
+public interface IHashService
+{
+    Task<string> Hash(string fingerprint, byte [] salt);
+
+    Task<bool> Verify(string fingerprint, byte [] salt, string hash);
+    byte[] CreateSalt();
+}
---
+++ App/Services/impl/HashService.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Konscious.Security.Cryptography;
+
+namespace App.Services.impl;
+
+public class HashService : IHashService
+{
+    public async Task<string> Hash(string fingerprint, byte [] salt)
+    {
+        var argon2 = new Argon2i(Encoding.UTF8.GetBytes(fingerprint));
+        argon2.Salt = salt;
+        argon2.Iterations = 2;
+        argon2.MemorySize = 65586;
+        argon2.Iterations = 4;
+        argon2.DegreeOfParallelism = 2;
+        var result = await argon2.GetBytesAsync(128);
+        return result.ToString();
+    }
+
+    public async Task<bool> Verify(string fingerprint, byte [] salt, string hash)
+    {
+        var newHash = await Hash(fingerprint, salt);
+        // byte[] newHashBytes = Encoding.UTF8.GetBytes(newHash);
+        return hash.Equals(newHash);
+    }
+
+    public byte[] CreateSalt()
+    {
+        var buffer = new byte[16];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(buffer);
+        return buffer;
+    }
+}
+
