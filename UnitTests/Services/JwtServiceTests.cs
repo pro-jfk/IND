eb24d3e45da1949ea7510cff84b1027fbbbfe6e7
JFK <jasrajkhedoe@outlook.com>
2023-01-08 21:32:53 +0100

    added tests for services

---
+++ UnitTests/Services/JwtServiceTests.cs
@@ -0,0 +1,135 @@
+﻿// using Microsoft.AspNetCore.Identity;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.IdentityModel.Tokens;
+// using System;
+// using System.IdentityModel.Tokens.Jwt;
+// using System.Security.Claims;
+// using System.Text;
+// using Xunit;
+//
+// namespace UnitTests.Services;
+//
+//     public class JwtServiceTests
+//     {
+//         private readonly IConfiguration _configuration;
+//         private readonly IdentityUser _user;
+//
+//         public JwtServiceTests()
+//         {
+//             // Set up the configuration with values needed for testing
+//             _configuration = new ConfigurationBuilder()
+//                 .AddInMemoryCollection(new[]
+//                 {
+//                     new KeyValuePair<string, string>("Jwt:Key", "test_key"),
+//                     new KeyValuePair<string, string>("Jwt:Subject", "test_subject"),
+//                     new KeyValuePair<string, string>("Jwt:Issuer", "test_issuer"),
+//                     new KeyValuePair<string, string>("Jwt:Audience", "test_audience")
+//                 })
+//                 .Build();
+//
+//             // Set up an IdentityUser for testing
+//             _user = new IdentityUser
+//             {
+//                 Id = "test_id",
+//                 UserName = "test_username",
+//                 Email = "test_email@example.com"
+//             };
+//         }
+//
+//         [Fact]
+//         public void CreateToken_ReturnsTokenAndExpiration()
+//         {
+//             // Arrange
+//             var jwtService = new JwtService(_configuration);
+//
+//             // Act
+//             var result = jwtService.CreateToken(_user);
+//
+//             // Assert
+//             Assert.NotNull(result.Token);
+//             Assert.True(result.Expiration > DateTime.UtcNow);
+//         }
+//
+//         [Fact]
+//         public void CreateToken_TokenIsValidJwt()
+//         {
+//             // Arrange
+//             var jwtService = new JwtService(_configuration);
+//
+//             // Act
+//             var result = jwtService.CreateToken(_user);
+//
+//             // Assert
+//             var tokenHandler = new JwtSecurityTokenHandler();
+//             tokenHandler.ValidateToken(result.Token, new TokenValidationParameters(), out SecurityToken securityToken);
+//             Assert.IsType<JwtSecurityToken>(securityToken);
+//         }
+//
+//         [Fact]
+//         public void CreateToken_TokenContainsExpectedClaims()
+//         {
+//             // Arrange
+//             var jwtService = new JwtService(_configuration);
+//
+//             // Act
+//             var result = jwtService.CreateToken(_user);
+//
+//             // Assert
+//             var tokenHandler = new JwtSecurityTokenHandler();
+//             var claimsPrincipal = tokenHandler.ValidateToken(result.Token, new TokenValidationParameters(),
+//                 out SecurityToken securityToken);
+//             Assert.Equal(_configuration["Jwt:Subject"], claimsPrincipal.FindFirst(JwtRegisteredClaimNames.Sub).Value);
+//             Assert.Equal(_user.Id, claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier).Value);
+//             Assert.Equal(_user.UserName, claimsPrincipal.FindFirst(ClaimTypes.Name).Value);
+//             Assert.Equal(_user.Email, claimsPrincipal.FindFirst(ClaimTypes.Email).Value);
+//         }
+//
+//         [Fact]
+//         public void CreateToken_TokenExpiresAfterExpectedDuration()
+//         {
+//             // Arrange
+//             var jwtService = new JwtService(_configuration);
+//             var expectedExpiration = DateTime.UtcNow.AddMinutes(JwtService.EXPIRATION_MINUTES);
+//
+//             // Act
+//             var result = jwtService.CreateToken(_user);
+//
+//             // Assert
+//             Assert.True(result.Expiration - expectedExpiration < TimeSpan.FromSeconds(1));
+//         }
+//
+//         [Fact]
+//         public void CreateToken_TokenHasExpectedIssuerAndAudience()
+//         {
+//             // Arrange
+//             var jwtService = new JwtService(_configuration);
+//
+//             // Act
+//             var result = jwtService.CreateToken(_user);
+//
+//             // Assert
+//             var tokenHandler = new JwtSecurityTokenHandler();
+//             var securityToken = tokenHandler.ReadToken(result.Token) as JwtSecurityToken;
+//             Assert.Equal(_configuration["Jwt:Issuer"], securityToken.Issuer);
+//             Assert.Equal(_configuration["Jwt:Audience"], securityToken.Audience);
+//         }
+//
+//         [Fact]
+//         public void CreateToken_TokenIsSignedWithExpectedKey()
+//         {
+//             // Arrange
+//             var jwtService = new JwtService(_configuration);
+//
+//             // Act
+//             var result = jwtService.CreateToken(_user);
+//
+//             // Assert
+//             var tokenHandler = new JwtSecurityTokenHandler();
+//             tokenHandler.ValidateToken(result.Token, new TokenValidationParameters
+//             {
+//                 IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]))
+//             }, out SecurityToken securityToken);
+//         }
+//     }
+//
+//
---
+++ UnitTests/Services/MessageServiceTests.cs
@@ -0,0 +1,130 @@
+﻿using System.Linq.Expressions;
+using App.Models;
+using App.Responses;
+using App.Services;
+using App.Services.impl;
+using AutoMapper;
+using Core.Entities;
+using Core.Exceptions;
+using Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using UnitTests.Mocks;
+using UnitTests.Mocks.Repositories;
+using UnitTests.Mocks.Services;
+
+
+namespace UnitTests.Services;
+
+public class MessageServiceTests 
+{
+    private readonly Mock<IMessageRepository> _mockMessageRepository;
+    private readonly Mock<IMapper> _mockMapper;
+    private readonly MessageService _messageService;
+
+    public MessageServiceTests()
+    {
+        _mockMessageRepository = new Mock<IMessageRepository>();
+        var mockCustomerMessageService = new Mock<ICustomerMessageService>();
+        _mockMapper = new Mock<IMapper>();
+        _messageService = new MessageService(_mockMessageRepository.Object, _mockMapper.Object,
+            mockCustomerMessageService.Object);
+    }
+    [Fact]
+    public async Task GetMessage_Successful()
+    {
+        // Arrange
+        var customerId = 1;
+        var message = new Message
+        {
+            Id = 1,
+            CustomerId = customerId,
+            FileURL = "filehostserver.com",
+            Type = "group-invite"
+        };
+        var messageResponse = new MessageResponse
+        {
+            Id = 1,
+            FileURL = "filehostserver.com",
+            Type = "group-invite"
+        };
+        _mockMessageRepository.Setup(x => x.GetFirstASync(m => m.CustomerId == customerId)).ReturnsAsync(message);
+        _mockMapper.Setup(x => x.Map<MessageResponse>(message)).Returns(messageResponse);
+
+        // Act
+        var result = await _messageService.GetMessage(customerId);
+
+        // Assert
+        _mockMessageRepository.Verify(x => x.GetFirstASync(m => m.CustomerId == customerId), Times.Once);
+        _mockMapper.Verify(x => x.Map<MessageResponse>(message), Times.Once);
+        Assert.Equal(messageResponse, result);
+        Assert.Equal(messageResponse.Type, message.Type);
+    }
+    
+    // [Fact]
+    // public async Task GetMessage_MessageDoesntExist_ReturnsException()
+    // {
+    //     //Arrange
+    //     // var messageRepository = new MockMessageRepository().MockGetByIdInvalid();
+    //     // var mockMapper = new Mock<IMapper>();
+    //     // var mockCustomerMessageService = new Mock<ICustomerMessageService>();
+    //     // var messageService = new MessageService(messageRepository.Object, mockMapper.Object, mockCustomerMessageService.Object);
+    //     _mockMessageRepository.Setup(x => x.GetFirstASync(m => m.CustomerId == 1)).ReturnsAsync(message);
+    //     _mockMapper.Setup(x => x.Map<MessageResponse>(message)).Returns(messageResponse);
+    //     
+    //     //Act
+    //     var result =   messageService.GetMessage(1);
+    //     
+    //     //Assert
+    //     await Assert.ThrowsAsync<ResourceNotFoundException>( async () => await result);
+    //
+    // }
+    [Fact]
+    public async Task GetMessage_Should_Fail()
+    {
+        // Arrange
+        var customerId = 1;
+        _mockMessageRepository.Setup(x => x.GetFirstASync(m => m.CustomerId == customerId)).ReturnsAsync((Message)null);
+
+        // Act
+        var result = await _messageService.GetMessage(customerId);
+
+        // Assert
+        _mockMessageRepository.Verify(x => x.GetFirstASync(m => m.CustomerId == customerId), Times.Once);
+        _mockMapper.Verify(x => x.Map<MessageResponse>(It.IsAny<Message>()), Times.Never);
+        Assert.Null(result);
+    }
+    [Fact]
+    public async Task GetMessage_MappingFails()
+    {
+     
+        // Arrange
+        var customerId = 1;
+        var message = new Message();
+        _mockMessageRepository.Setup(x => x.GetFirstASync(m => m.CustomerId == customerId)).ReturnsAsync(message);
+        _mockMapper.Setup(x => x.Map<MessageResponse>(message)).Throws<Exception>();
+
+        // Act
+        await Assert.ThrowsAsync<Exception>(() => _messageService.GetMessage(customerId));
+
+        // Assert
+        _mockMessageRepository.Verify(x => x.GetFirstASync(m => m.CustomerId == customerId), Times.Once);
+        _mockMapper.Verify(x => x.Map<MessageResponse>(message), Times.Once);
+    }
+
+
+    [Fact]
+    public void AddMessage()
+    {
+        
+    }
+
+    [Fact]
+    public void PutMessage_PrintAndReceivedStatus()
+    {
+        
+    }
+  
+}
+//Arrange
+//Act
+//Assert
