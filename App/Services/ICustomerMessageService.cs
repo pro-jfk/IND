9f2c8071d0db4a34f2901a8175adb7690b68f279
JFK <jasrajkhedoe@outlook.com>
2023-01-09 14:11:40 +0100

    removed use

--- App/Services/ICustomerMessageService.cs
+++ App/Services/ICustomerMessageService.cs
@@ -1,17 +1,17 @@
-﻿using App.Models;
-using App.Responses;
-using Core.Entities;
-
-namespace App.Services;
-
-public interface ICustomerMessageService
-{
-    Task<CustomerMessageResponse> CreateCustomerMessage( CreateCustomerMessage createCustomerMessage);
-    Task<CustomerMessageResponse> GetCustomerMessage(int customerId, int messageId);
-    Task<IEnumerable<CustomerMessageResponse>> GetCustomerMessages();
-    Task<CustomerMessageResponse> UpdateCustomerMessagePrintJob(int customerId, int messageId, bool statusPrinted );
-    Task<CustomerMessageResponse> UpdateCustomerMessageReceived(int customerId, int messageId, DateTime dateTime, bool statusReceived);
-    Task<CustomerMessageResponse> DeleteCustomerMessage(int customerId, int messageId);
-
-    
-}
+﻿// using App.Models;
+// using App.Responses;
+// using Core.Entities;
+//
+// namespace App.Services;
+//
+// public interface ICustomerMessageService
+// {
+//     Task<CustomerMessageResponse> CreateCustomerMessage( CreateCustomerMessage createCustomerMessage);
+//     Task<CustomerMessageResponse> GetCustomerMessage(int customerId, int messageId);
+//     Task<IEnumerable<CustomerMessageResponse>> GetCustomerMessages();
+//     Task<CustomerMessageResponse> UpdateCustomerMessagePrintJob(int customerId, int messageId, bool statusPrinted );
+//     Task<CustomerMessageResponse> UpdateCustomerMessageReceived(int customerId, int messageId, DateTime dateTime, bool statusReceived);
+//     Task<CustomerMessageResponse> DeleteCustomerMessage(int customerId, int messageId);
+//
+//     
+// }
