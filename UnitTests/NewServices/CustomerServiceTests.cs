4b5bc7b21c3f6a584dbc6a3b8a84fadb9c1004b9
JFK <jasrajkhedoe@outlook.com>
2023-01-15 22:47:35 +0100

    refactored from old

---
+++ UnitTests/NewServices/CustomerServiceTests.cs
@@ -0,0 +1,142 @@
+﻿using System.Text;
+using App.MappingProfile;
+using App.Models;
+using App.Services;
+using App.Services.impl;
+using AutoMapper;
+using Core.Exceptions;
+using Data;
+using Data.Repositories.Impl;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTests.NewServices;
+
+public class CustomerServiceTests
+{
+
+    [Fact]
+    public async Task CreateCustomer_Successful()
+    {
+        // Arrange
+        DbContextOptions<IndContext> options = new DbContextOptionsBuilder<IndContext>()
+            .UseInMemoryDatabase(databaseName: "CreateCustomer_AddCustomer")
+            .Options;
+        using (IndContext context = new IndContext(options))
+        {
+            var hashedFingerprint = Encoding.UTF8.GetBytes("fingerprintandsalt");
+            var hashServiceMock = new Mock<IHashService>();
+            hashServiceMock.Setup(h => h.Hash(It.IsAny<string>(), It.IsAny<byte[]>())).ReturnsAsync(hashedFingerprint);
+
+
+            var customerRepository = new CustomerRepository(context);
+            var config = new MapperConfiguration(cfg => { cfg.AddProfile<CustomerProfile>(); });
+            var mapper = new Mapper(config);
+            var customerService = new CustomerService(customerRepository, mapper, hashServiceMock.Object);
+
+            var customer = new CreateCustomer
+            {
+                Id = 1234567890,
+                FirstName = "John",
+                LastName = "Doe",
+                LanguagesSpoken = "English",
+                Origin = "Canada",
+                PhoneNumber = "+1-613-555-0149 ",
+                EmergencyShelterId = 1
+            };
+            // Act
+            var createdCustomer = await customerService.CreateCustomer(customer);
+            var getCustomer = await customerService.GetCustomer(1234567890);
+            // Assert
+            Assert.Equal(customer.Id, getCustomer.Id);
+            Assert.Equal(customer.Origin, getCustomer.Origin);
+        }
+    }
+
+    [Fact]
+        public async Task DeleteMessage_Successful()
+        {
+            // Arrange
+            DbContextOptions<IndContext> options = new DbContextOptionsBuilder<IndContext>()
+                .UseInMemoryDatabase(databaseName: "DeleteMessage_DeletesCorrectly")
+                .Options;
+            using (IndContext context = new IndContext(options))
+            {
+                var hashedFingerprint = Encoding.UTF8.GetBytes("fingerprintandsalt");
+                var hashServiceMock = new Mock<IHashService>();
+                hashServiceMock.Setup(h => h.Hash(It.IsAny<string>(), It.IsAny<byte []>())).ReturnsAsync(hashedFingerprint);
+            
+                var customerRepository = new CustomerRepository(context);
+                var config = new MapperConfiguration(cfg => {
+                    cfg.AddProfile<CustomerProfile>();
+                });
+                var mapper = new Mapper(config);
+                var customerService = new CustomerService(customerRepository, mapper, hashServiceMock.Object);
+            
+                var customer = new CreateCustomer
+                {
+                    Id = 1234567890,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    LanguagesSpoken = "English",
+                    Origin = "Canada",
+                    PhoneNumber = "+1-613-555-0149 ",
+                    EmergencyShelterId = 1
+                };
+                await customerService.CreateCustomer(customer);
+                // Act
+                await customerService.DeleteCustomer(1234567890);
+                // Assert
+            
+                await Assert.ThrowsAsync<ResourceNotFoundException>(async () => await customerRepository.GetFirstASync(m => m.Id == 1));
+            }
+        }
+        
+        
+        [Fact]
+        public async Task UpdateCustomer_ChangeFirstName_Successful()
+        {
+            // Arrange
+            DbContextOptions<IndContext> options = new DbContextOptionsBuilder<IndContext>()
+                .UseInMemoryDatabase(databaseName: "UpdateCustomer_ChangeId_Successful")
+                .Options;
+            using (IndContext context = new IndContext(options))
+            {
+                var hashedFingerprint = Encoding.UTF8.GetBytes("fingerprintandsalt");
+                var hashServiceMock = new Mock<IHashService>();
+                hashServiceMock.Setup(h => h.Hash(It.IsAny<string>(), It.IsAny<byte []>())).ReturnsAsync(hashedFingerprint);
+            
+                var customerRepository = new CustomerRepository(context);
+                var config = new MapperConfiguration(cfg => {
+                    cfg.AddProfile<CustomerProfile>();
+                });
+                var mapper = new Mapper(config);
+                var customerService = new CustomerService(customerRepository, mapper, hashServiceMock.Object);
+            
+                var customer = new CreateCustomer
+                {
+                    Id = 1234567890,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    LanguagesSpoken = "English",
+                    Origin = "Canada",
+                    PhoneNumber = "+1-613-555-0149 ",
+                    EmergencyShelterId = 1
+                };
+                await customerService.CreateCustomer(customer);
+                
+                
+                //Act
+                var existingCustomer = await customerRepository.GetFirstASync(c => c.Id == 1234567890);
+                existingCustomer.FirstName = "Tom";
+                await customerRepository.UpdateAsync(existingCustomer);
+                var updatedCustomer = await customerService.GetCustomer(1234567890);
+             
+              
+                // Assert
+                Assert.Equal("Tom", updatedCustomer.FirstName);
+            }
+        }
+
+    
+    
+}
---
+++ UnitTests/NewServices/MessageServiceTests.cs
@@ -0,0 +1,241 @@
+﻿using App.MappingProfile;
+using App.Models;
+using App.Services.impl;
+using AutoMapper;
+using Core.Entities;
+using Core.Exceptions;
+using Data;
+using Data.Repositories.Impl;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTests.NewServices;
+
+public class MessageServiceTests
+{
+    [Fact]
+    public async Task CreateMessage_AddsMessage()
+    {
+        // Arrange
+        DbContextOptions<IndContext> options = new DbContextOptionsBuilder<IndContext>()
+            .UseInMemoryDatabase(databaseName: "CreateMessage_AddsMessage")
+            .Options;
+        using (IndContext context = new IndContext(options))
+        {
+            var messageRepository = new MessageRepository(context);
+            var config = new MapperConfiguration(cfg => {
+                cfg.AddProfile<MessageProfile>();
+            });
+            var mapper = new Mapper(config);
+            var messageService = new MessageService(messageRepository, mapper);
+
+            var createMessage = new CreateMessage
+            {
+                Id = 1,
+                CustomerId = 1,
+                FileURL = "fileshostingserver.com",
+                Type = "invite"
+                
+            };
+            // Act
+            var createdMessage = await messageService.CreateMessage(createMessage);
+            // Assert
+            Assert.Equal(1, createdMessage.Id);
+            Assert.Equal("invite", createdMessage.Type);
+        }
+    }
+
+    [Fact]
+    public async Task GetMessage_ReturnsCorrectMessage()
+    {
+        // Arrange
+        DbContextOptions<IndContext> options = new DbContextOptionsBuilder<IndContext>()
+            .UseInMemoryDatabase(databaseName: "GetMessage_ReturnsCorrectMessage")
+            .Options;
+        using (IndContext context = new IndContext(options))
+        {
+            var messageRepository = new MessageRepository(context);
+            var config = new MapperConfiguration(cfg => { cfg.AddProfile<MessageProfile>(); });
+            var mapper = new Mapper(config);
+            var messageService = new MessageService(messageRepository, mapper);
+
+            var message = new CreateMessage
+            {
+                Id = 1,
+                CustomerId = 1,
+                FileURL = "fileshostingserver.com",
+                Type = "invite"
+            };
+            await messageService.CreateMessage(message);
+            // Act
+            var result = await messageService.GetMessage(1);
+            // Assert
+            Assert.Equal(1, result.Id);
+            Assert.Equal(message.Type, result.Type);
+            Assert.Equal(message.FileURL, result.FileURL);
+        }
+    }
+
+    [Fact]
+        public async Task GetMessagesForCustomer_ReturnsCorrectMessages()
+        {
+            // Arrange
+            DbContextOptions<IndContext> options = new DbContextOptionsBuilder<IndContext>()
+                .UseInMemoryDatabase(databaseName: "GetMessagesForCustomer_ReturnsCorrectMessages")
+                .Options;
+            using (IndContext context = new IndContext(options))
+            {
+                var messageRepository = new MessageRepository(context);
+                var config = new MapperConfiguration(cfg => {
+                    cfg.AddProfile<MessageProfile>();
+                });
+                var mapper = new Mapper(config);
+                var messageService = new MessageService(messageRepository, mapper);
+
+                var message1 = new CreateMessage
+                {
+                    Id = 1,
+                    CustomerId = 1,
+                    FileURL = "fileshostingserver.com",
+                    Type = "invite"
+                };
+                var message2 = new CreateMessage
+                {
+                    Id = 2,
+                    CustomerId = 2,
+                    FileURL = "fileshostingserver.com",
+                    Type = "invite"
+                    
+                };
+                var message3 = new CreateMessage
+                {
+                    Id = 3,
+                    CustomerId = 2,
+                    FileURL = "fileshostingserver.com",
+                    Type = "invite"
+                };
+                await messageService.CreateMessage(message1);
+                await messageService.CreateMessage(message2);
+                await messageService.CreateMessage(message3);
+                // Act
+                var result = (await messageService.GetMessagesForCustomer(2)).ToList();
+                // Assert
+                Assert.Equal(2, result.Count());
+                Assert.Equal(2, result.ElementAt(0).Id);
+                Assert.Equal(3, result.ElementAt(1).Id);
+
+            }
+        }
+
+
+
+        [Fact]
+    public async Task UpdateMessageReceivedStatus_UpdatesStatus()
+    {
+        // Arrange
+        DbContextOptions<IndContext> options = new DbContextOptionsBuilder<IndContext>()
+            .UseInMemoryDatabase(databaseName: "UpdateMessageReceivedStatus_UpdatesStatus")
+            .Options;
+        using (IndContext context = new IndContext(options))
+        {
+            var messageRepository = new MessageRepository(context);
+            var config = new MapperConfiguration(cfg => {
+                cfg.AddProfile<MessageProfile>();
+            });
+            var mapper = new Mapper(config);
+            var messageService = new MessageService(messageRepository, mapper);
+    
+            var message = new CreateMessage
+            {
+                Id = 1,
+                CustomerId = 1,
+                StatusReceived = false,
+                FileURL = "fileshostingserver.com",
+                Type = "invite"
+            };
+            await messageService.CreateMessage(message);
+            var dateReceived = DateTime.Now;
+            var statusReceived = true;
+            
+            // Act
+            await messageService.UpdateMessageReceived(1, 1, dateReceived, statusReceived);
+            var result = await messageRepository.GetFirstASync(m => m.Id == 1);
+            var test = await messageService.GetMessage(1);
+            
+            // Assert
+            Assert.Equal(dateReceived, result.DateReceived);
+            Assert.Equal(statusReceived, result.StatusReceived);
+        }
+    }
+    
+    [Fact]
+    public async Task UpdateMessagePrintjob_UpdatesPrintJob()
+    {
+        // Arrange
+        DbContextOptions<IndContext> options = new DbContextOptionsBuilder<IndContext>()
+            .UseInMemoryDatabase(databaseName: "UpdateMessagePrintjob_UpdatesPrintJob")
+            .Options;
+        using (IndContext context = new IndContext(options))
+        {
+            var messageRepository = new MessageRepository(context);
+            var config = new MapperConfiguration(cfg => {
+                cfg.AddProfile<MessageProfile>();
+            });
+            var mapper = new Mapper(config);
+            var messageService = new MessageService(messageRepository, mapper);
+    
+            var message = new CreateMessage
+            {
+                Id = 1,
+                CustomerId = 1,
+                TimesPrinted = 0,
+                StatusPrinted = false,
+                FileURL = "fileshostingserver.com",
+                Type = "message"
+            };
+            await messageService.CreateMessage(message);
+            var statusPrinted = true;
+            // Act
+            await messageService.UpdateMessagePrintJob(1, 1, statusPrinted);
+
+            var result = await messageRepository.GetFirstASync(m => m.Id == 1);
+            // Assert
+            Assert.Equal(1, result.TimesPrinted);
+            Assert.Equal(statusPrinted, result.StatusPrinted);
+        }
+    }
+    
+    [Fact]
+    public async Task DeleteMessage_DeletesCorrectly()
+    {
+        // Arrange
+        DbContextOptions<IndContext> options = new DbContextOptionsBuilder<IndContext>()
+            .UseInMemoryDatabase(databaseName: "DeleteMessage_DeletesCorrectly")
+            .Options;
+        using (IndContext context = new IndContext(options))
+        {
+            var messageRepository = new MessageRepository(context);
+            var config = new MapperConfiguration(cfg => {
+                cfg.AddProfile<MessageProfile>();
+            });
+            var mapper = new Mapper(config);
+            var messageService = new MessageService(messageRepository, mapper);
+
+            var message = new CreateMessage()
+            {
+                Id = 1,
+                CustomerId = 1,
+                FileURL = "fileshostingserver.com",
+                Type = "invite"
+            };
+            await messageService.CreateMessage(message);
+            // Act
+            await messageService.DeleteMessage(1);
+            // Assert
+            
+            await Assert.ThrowsAsync<ResourceNotFoundException>(async () => await messageRepository.GetFirstASync(m => m.Id == 1));
+        }
+    }
+
+
+
+}
